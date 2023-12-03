ede6745e3476ab0bcef1151c951ca4c9d183fcde
Shimon Lyons <shimon.lyons@apexgroup.com>
2023-12-03 21:31:39 +0200

    Extra words, fixed bugs

--- kosherify.js
+++ kosherify.js
@@ -1,24 +1,27 @@
 let conversion = {
+    "Advent calendar": "Shul Calendar",
     "Advent": "Shul Calendar",
     "Christmas": "Chanuka",
     "Santa": "The Rebbe",
+    "an elf": "a Chossid",
     "elf": "chossid",
     "elves'": "chassidim's",
     "elves": "chassidim",
-    "reindeer": "Moshiach's donkey",
+    "reindeer": "Moshiach's donkey", // Unfortunately we can't distinguish singular and plural
     "sleigh": "wagon",
     "bells": "accordions",
     "bell": "accordion",
     "stars": "Chanuka candles",
     "star": "Chanuka candle",
     "magic": "kabbalah",
-    "magical": "kabbalistic"
+    "magical": "kabbalistic",
+    "magician": "kabbalist"
 }
 
 // Optimisations so we don't do this every time the function is called
 conversion = Object.entries(conversion).map(([from, to]) => [new RegExp(`\\b${from}${/\w$/.test(from) ? '\\b' : ''}`, 'ig'), to]);
 // Add stars as a special case, not requiring word boundaries
-conversion.push(['*', '🕎'])
+conversion.push(['*', '🕎']); // I tried 🕯️ but it didn't look good
 
 /**
  * 
